8b47cc9a14ef8a21d3462fa0ec13fb6d7c670748
Lucas Salbego <lucas.salbego99@edu.pucrs.br>
2022-10-12 09:37:03 -0300

    tilemaps

--- PlayerCharacter.gd
+++ PlayerCharacter.gd
@@ -2,17 +2,8 @@ extends KinematicBody2D
 
 export var speed := 200.0
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func _physics_process(_delta):
 	var input_vector := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var movement := input_vector.normalized()
 	
 	move_and_slide(speed * movement)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
